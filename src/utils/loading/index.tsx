f5ad2551eeb88fc15985b6e00fe70857c06c200e
李爽 <lishuang5@yonyou.com>
2025-03-03 23:16:43 +0800

    feat: axios 拦截器封装 - 添加loading 2

--- src/utils/loading/index.tsx
+++ src/utils/loading/index.tsx
@@ -1,24 +1,42 @@
 import { createRoot } from 'react-dom/client';
 import Loading from './loading';
+import './loading.less';
 
 let count = 0;
 
+// export const showLoading = () => {
+//   if (count === 0) {
+//     const loading = document.createElement('div');
+//     loading.setAttribute('id', 'loading');
+//     document.body.appendChild(loading);
+//     createRoot(loading).render(<Loading tip="Loading" />);
+//   }
+//   count++;
+// };
+
+// export const hideLoading = () => {
+//     console.log('count', count);
+//   if (count <= 0) return;
+//   count--;
+//   if(count === 0) {
+//     const loading = document.getElementById('loading') as HTMLElement;
+//     document.body.removeChild(loading);
+//   }
+// };
+
 export const showLoading = () => {
   if (count === 0) {
-    const loading = document.createElement('div');
-    loading.setAttribute('id', 'loading');
-    document.body.appendChild(loading);
-    createRoot(loading).render(<Loading tip="Loading" />);
+    const loading = document.getElementById('loading') as HTMLElement;
+    loading?.style.setProperty('display', 'flex');
   }
   count++;
 };
 
 export const hideLoading = () => {
-    console.log('count', count);
   if (count <= 0) return;
   count--;
-  if(count === 0) {
+  if (count === 0) {
     const loading = document.getElementById('loading') as HTMLElement;
-    document.body.removeChild(loading);
+    loading?.style.setProperty('display', 'none');
   }
 };
