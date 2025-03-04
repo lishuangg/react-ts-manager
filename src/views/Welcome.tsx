75bd54560b67acdb1a053acd98eda38cf0e9bb70
lishuangg <1241982864@qq.com>
2025-03-05 00:29:29 +0800

    feat: 金额格式化 & 日期格式化

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,8 +1,17 @@
 import storage from "@/utils/storage";
 import { Button } from "antd";
+import {formatMoney, toLocalDate, formatDate} from "@/utils/index";
 
 export default function Welcome() {
-  const handleClick = (type: number) => {
+  const handleClick = () => {
+    // const money = formatMoney('142353654.224');
+    // console.log(money);
+    // let data = toLocalDate(new Date(), 'yyyy-MM-dd');
+    // console.log(data);
+    let data = formatDate(new Date(), 'yyyy-MM-dd');
+    console.log(data);
+  }
+  const handleStorage= (type: number) => {
     if(type === 1) {
       storage.set("test", "test");
       storage.set("test2", { name: "test" , age: 18 });
@@ -19,10 +28,11 @@ export default function Welcome() {
     <div>
       <h1>Welcome</h1>
       <p>
-        <Button onClick={() => handleClick(1)}>写入值</Button>
-        <Button onClick={() => handleClick(2)}>读取值</Button>
-        <Button onClick={() => handleClick(3)}>删除</Button>
-        <Button onClick={() => handleClick(4)}>清空</Button>
+        <Button onClick={() => handleClick()}>点击事件</Button>
+        <Button onClick={() => handleStorage(1)}>写入值</Button>
+        <Button onClick={() => handleStorage(2)}>读取值</Button>
+        <Button onClick={() => handleStorage(3)}>删除</Button>
+        <Button onClick={() => handleStorage(4)}>清空</Button>
       </p>
     </div>
   );
