861a9911bc92998574b024709e680f11a6b7dca0
lishuangg <1241982864@qq.com>
2025-03-06 00:12:35 +0800

    feat:  定制主题颜色

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import { BrowserRouter, RouterProvider } from 'react-router';
+import { ConfigProvider } from 'antd';
 import router from './router';
 // import Router from './router';
 import './App.css';
 
 function App() {
-    // 组件路由
-    // return <BrowserRouter><Router /></BrowserRouter>
-    // api路由 - 推荐 可以使用loader action
-    return <RouterProvider router={router} />;
+  // 组件路由
+  // return <BrowserRouter><Router /></BrowserRouter>
+  // api路由 - 推荐 可以使用loader action
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#ed6c00'
+        }
+      }}
+    >
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  );
 }
 
 export default App;
