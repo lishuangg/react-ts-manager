9fc2676fda325fae593c5bf51d4dfd5aa544c743
lishuangg <1241982864@qq.com>
2025-03-11 17:33:21 +0800

    feat: 添加layout & 水印

---
+++ src/layout/index.tsx
@@ -0,0 +1,80 @@
+import React, { useEffect } from 'react';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Layout, Menu, theme, Watermark } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`
+}));
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG }
+  } = theme.useToken();
+
+  // 模拟水印无法删除的原理
+  useEffect(() => {
+    const dom = document.getElementById('content') as HTMLDivElement;
+
+    // MutationObserver 接口提供了监视对 DOM 树所做更改的能力。它被设计为旧的 Mutation Events 功能的替代品，该功能是 DOM3 Events 规范的一部分。
+    const observer = new MutationObserver(function (mutationsList, observer) {
+      console.log('mutationsList', mutationsList);
+      observer.disconnect(); // 停止观察 避免无限循环
+      for (const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          console.log('A child node has been added or removed.');
+          const span = document.createElement('span');
+          span.innerText = 'hello';
+          dom.appendChild(span);
+          observer.observe(dom, config); // 重新开始观察
+        } else if (mutation.type === 'attributes') {
+          console.log('The ' + mutation.attributeName + ' attribute was modified.');
+        }
+      }
+    });
+    const config = { attributes: true, childList: true, subtree: true };
+    observer.observe(dom, config);
+  }, []);
+
+  return (
+    <Watermark content="Ant Design">
+      <Layout>
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <div className="demo-logo-vertical" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: '24px 16px 0' }}>
+            <div
+              style={{
+                padding: 24,
+                minHeight: 360,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG
+              }}
+              id="content"
+            >
+              <span>content</span>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©{new Date().getFullYear()} Created by Ant UED</Footer>
+        </Layout>
+      </Layout>
+    </Watermark>
+  );
+};
+
+export default App;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,15 +3,25 @@ import Login from '@/views/Login/Login';
 import Welcome from '@/views/Welcome';
 import Error404 from '@/views/404';
 import Error403 from '@/views/403';
+import Layout from '@/layout';
 
 const routers = [
     {
         path: '/',
-        element: <Welcome />
+        element: <Navigate to="/welcome" />
         },
     {
         path: '/login',
         element: <Login />
+    },
+    {
+        element: <Layout />,
+        children: [
+            {
+                path: '/welcome',
+                element: <Welcome />
+            },
+        ]
     },
     {
         path: '*',
